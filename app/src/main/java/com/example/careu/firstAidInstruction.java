bddb9f0851341f5e7d134b699119f343ee6043d9
Pasanbashz <bashzpasan@gmail.com>
2020-10-09 15:29:01 +0530

    FirstAid_instructions

--- app/src/main/java/com/example/careu/firstAidInstruction.java
+++ app/src/main/java/com/example/careu/firstAidInstruction.java
@@ -2,7 +2,9 @@ package com.example.careu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class firstAidInstruction extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class firstAidInstruction extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_aid_instruction);
+    }
+
+    public void btn1(View view) {
+        Intent i= new Intent(this, Pop1.class);
+        startActivity(i);
+    }
+
+    public void btn2(View view) {
+        Intent i= new Intent(this, Pop2.class);
+        startActivity(i);
+    }
+
+    public void btn3(View view) {
+        Intent i= new Intent(this, Pop3.class);
+        startActivity(i);
     }
 }
