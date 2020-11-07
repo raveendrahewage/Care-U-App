880f32a604817aa44011d50a2e72487730c4a7ab
LManjitha <lmanjithah@gmail.com>
2020-11-07 10:57:12 +0530

    add upload ID feature

--- app/src/main/java/com/example/careu/registrationPage.java
+++ app/src/main/java/com/example/careu/registrationPage.java
@@ -49,7 +49,7 @@ public class registrationPage extends AppCompatActivity {
     //TextView _fnametv,_lnametv,_emailtv,_pnumtv,_usernametv,_pwdtv,_nictv,_addresstv,_r1_numtv,_r2_numtv,_r3_numtv;
     AwesomeValidation awesomeValidation;
     Button selectPics1,selectPics2,upload1,upload2,btnReg;
-    Bitmap bitmap;
+    Bitmap bitmap1,bitmap2;
     Spinner _gender;
     DatePicker _birthDay;
 
@@ -97,8 +97,6 @@ public class registrationPage extends AppCompatActivity {
       //  selectPics.setOnClickListener(new View.OnClickListener() {
         selectPics1 = findViewById(R.id.btnSelectPic1);
         selectPics2 = findViewById(R.id.btnSelectPic2);
-        upload1 = findViewById(R.id.btnUpload1);
-        upload2 = findViewById(R.id.btnUpload2);
         btnReg = findViewById(R.id.btnReg);
        // nicNum = findViewById(R.id.nic);
         final String uploadUrl = "http://10.0.2.2/careu-php/uploadID.php";
@@ -139,86 +137,10 @@ public class registrationPage extends AppCompatActivity {
                 Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
                 intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE,true);
                 intent.setType("image/*");
-                startActivityForResult(intent,1);
-            }
-        });
-
-        upload1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                StringRequest stringRequest = new StringRequest(Request.Method.POST, uploadUrl, new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        try {
-                            JSONObject jsonObject = new JSONObject(response);
-                            String Response = jsonObject.getString("response");
-                            Toast.makeText(registrationPage.this,Response,Toast.LENGTH_LONG).show();
-
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-
-                    }
-                }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-
-                    }
-                })
-
-                {
-                    @Override
-                    protected Map<String, String> getParams() throws AuthFailureError {
-                        Map <String,String> params = new HashMap<>();
-                        params.put("name",_nic.getText().toString().trim()+" 1");
-                        params.put("image",imageToString(bitmap));
-
-
-                        return params;
-                    }
-                };
-                MySingleton.getInstance(registrationPage.this).addToRequestQue(stringRequest);
-            }
-        });
-
-        upload2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                StringRequest stringRequest = new StringRequest(Request.Method.POST, uploadUrl, new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        try {
-                            JSONObject jsonObject = new JSONObject(response);
-                            String Response = jsonObject.getString("response");
-                            Toast.makeText(registrationPage.this,Response,Toast.LENGTH_LONG).show();
-
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-
-                    }
-                }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-
-                    }
-                })
-
-                {
-                    @Override
-                    protected Map<String, String> getParams() throws AuthFailureError {
-                        Map <String,String> params = new HashMap<>();
-                        params.put("name",_nic.getText().toString().trim()+" 2");
-                        params.put("image",imageToString(bitmap));
-
-                        return params;
-                    }
-                };
-                MySingleton.getInstance(registrationPage.this).addToRequestQue(stringRequest);
+                startActivityForResult(intent,2);
             }
         });
     }
-
 
 
     @Override
@@ -228,7 +150,14 @@ public class registrationPage extends AppCompatActivity {
         if(requestCode==1 && resultCode== RESULT_OK && data != null){
             Uri path = data.getData();
             try {
-                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),path);
+                bitmap1 = MediaStore.Images.Media.getBitmap(getContentResolver(),path);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }else if(requestCode==2 && resultCode== RESULT_OK && data != null){
+            Uri path = data.getData();
+            try {
+                bitmap2 = MediaStore.Images.Media.getBitmap(getContentResolver(),path);
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -244,6 +173,7 @@ public class registrationPage extends AppCompatActivity {
     }
 
     public void register(View view) {
+        String uploadUrl = "http://10.0.2.2/careu-php/uploadID.php";
         awesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
         String s;
         String fname = _fname.getText().toString();
@@ -259,8 +189,8 @@ public class registrationPage extends AppCompatActivity {
        // number(phone,R.id.pnum);
        awesomeValidation.addValidation(this,R.id.pnum,"[0]{1}[7]{1}[1||2||5||6||7||8]{1}[0-9]{7}$",R.string.invalid_number1);
 
-        String username = _username.getText().toString();
-        awesomeValidation.addValidation(this,R.id.usrname, RegexTemplate.NOT_EMPTY,R.string.Invalid_user_name);
+        final String username = _username.getText().toString();
+       awesomeValidation.addValidation(this,R.id.usrname, RegexTemplate.NOT_EMPTY,R.string.Invalid_user_name);
 
         String pwd = _pwd.getText().toString();
         awesomeValidation.addValidation(this,R.id.pwd,".{8,}",R.string.invalid_password);
@@ -316,87 +246,81 @@ public class registrationPage extends AppCompatActivity {
             awesomeValidation.addValidation(this,R.id.r3_num,"[0]{1}[7]{1}[1||2||5||6||7||8]{1}[0-9]{7}$",R.string.invalid_number1);
         }
         if (awesomeValidation.validate()) {
-//            Intent i = new Intent(this, loginPage.class);
-//            startActivity(i);
+         //   Intent i = new Intent(this, loginPage.class);
+           // startActivity(i);
             String type = "register";
 
             BackgroundWorker backgroundWorker = new BackgroundWorker(this);
             backgroundWorker.execute(type,fname,lname,email,phone,username,pwd,NIC,address,gender,dateOfBirth,r1,r1_num,r2,r2_num,r3,r3_num);
         }
 
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, uploadUrl, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    String Response = jsonObject.getString("response");
+                    Toast.makeText(registrationPage.this,Response,Toast.LENGTH_LONG).show();
 
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        })
+
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map <String,String> params = new HashMap<>();
+                params.put("userName",username);
+                params.put("name",_nic.getText().toString().trim()+"_1");
+                params.put("image",imageToString(bitmap1));
+
+                return params;
+            }
+        };
+        MySingleton.getInstance(registrationPage.this).addToRequestQue(stringRequest);
+
+        StringRequest stringRequest2 = new StringRequest(Request.Method.POST, uploadUrl, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    String Response = jsonObject.getString("response");
+                    Toast.makeText(registrationPage.this,Response,Toast.LENGTH_LONG).show();
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        })
+
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map <String,String> params = new HashMap<>();
+                params.put("userName",username);
+                params.put("name",_nic.getText().toString().trim()+"_2");
+                params.put("image",imageToString(bitmap2));
+
+                return params;
+            }
+        };
+        MySingleton.getInstance(registrationPage.this).addToRequestQue(stringRequest2);
     }
-//    public int number(String phone,TextView _ptv){
-//
-//        if (phone.isEmpty()){
-//            _ptv.setText("*Plase enter your Phone number");
-//            showAlertDialog("* Fill  Phone Number  Field*");
-//            return 0;
-//        } else if (phone.length()==10) {
-//                if (!((phone.contains("076") || phone.contains("071") || phone.contains("070") || phone.contains("077") || phone.contains("078") || phone.contains("072") || phone.contains("075")))) {
-//                    showAlertDialog("* phone number must be 070/071/072/075/076/077/078*");
-//                    _ptv.setText("phone number must be 070/071/072/075/076/077/078");
-//                    return 0;
-//                } else {
-//                    if(phone.startsWith("070")||phone.startsWith("071")||phone.startsWith("072")||phone.startsWith("075")||phone.startsWith("076")||phone.startsWith("077")||phone.startsWith("078")){
-//                        _ptv.setText("");
-//                        return 1;
-//                    }
-//                }
-//            }else if(phone.length()==9){
-//                if(!((phone.contains("76")||phone.contains("71")||phone.contains("70")||phone.contains("77")||phone.contains("78")||phone.contains("72")||phone.contains("75")))) {
-//                    showAlertDialog("* phone number must be 70/71/72/75/76/77/78*");
-//                    _ptv.setText("phone number must be 70/71/72/75/76/77/78");
-//                    return 0;
-//                }else{
-//                    if(phone.startsWith("70")||phone.startsWith("71")||phone.startsWith("72")||phone.startsWith("75")||phone.startsWith("76")||phone.startsWith("77")||phone.startsWith("70")){
-//                        _ptv.setText("");
-//                        return 1;
-//                    }
-//                }
-//            } else if (phone.length()!=10){
-//                showAlertDialog("*Wrong lenght of Phone number*");
-//                _ptv.setText("Wrong lenght of Phone number ");
-//                return 0;
-//            }
-//
-//        _ptv.setText("");
-//        return 1;
-//
-//
-//    }
-//    public void showAlertDialog(String s){
-//        AlertDialog.Builder builder1 = new AlertDialog.Builder(this);
-//        builder1.setMessage(s);
-//        builder1.setCancelable(true);
-//        AlertDialog alert11 = builder1.create();
-//        alert11.show();
-//
-//    }
-//    public int error (String er,TextView _tv,String type){
-//        if (er.isEmpty()){
-//            showAlertDialog("* Fill "+ type +"Field*");
-//            _tv.setText("*You must Enter "+type);
-//            return 0;
-//        }else{
-//            if (type.equals("NIC")){
-//                if (er.length()==10 && !((er.contains("v"))||(er.contains("V")))){
-//                    showAlertDialog("* Error "+ type +"Field*");
-//                    _tv.setText("*You must Enter 98900259v type NIC");
-//                    return 0;
-//                }else {
-//                    if (er.length()!=10){
-//                        showAlertDialog("* Invalid Size "+ type +"Field*");
-//                        _tv.setText("*you must enter 10digits");
-//                        return 0;
-//                    }
-//                }
-//            }
-//            _tv.setText("");
-//            return 1;
-//        }
-//
-//    }
+
 
    public void number (String s,int k) {
         if (s.length()==10){
