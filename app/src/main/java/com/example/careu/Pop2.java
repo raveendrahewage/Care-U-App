130e2a82ac24ec6ff57e087ce8adc068a88e8803
Pasanbashz <bashzpasan@gmail.com>
2020-11-08 20:02:25 +0530

    connect first-aid instructions with database

--- app/src/main/java/com/example/careu/Pop2.java
+++ app/src/main/java/com/example/careu/Pop2.java
@@ -1,16 +1,52 @@
 package com.example.careu;
 
-import android.app.Activity;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class Pop2  extends AppCompatActivity
+{
+    private static final String apiurl="http://10.0.2.2/careu-php/instruction2.php";
+    ListView lv;
 
 
-public class Pop2 extends Activity {
+    private static String name[];
+    private static String desig[];
+    private static String img[];
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.pop2_window);
 
         DisplayMetrics dm = new DisplayMetrics();
@@ -20,5 +56,150 @@ public class Pop2 extends Activity {
         int height = dm.heightPixels;
 
         getWindow().setLayout((int)(width*.8),(int)(height*.7));
+
+
+
+        lv=(ListView)findViewById(R.id.lv);
+
+        fetch_data_into_array(lv);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String s = lv.getItemAtPosition(position).toString();
+
+                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+    public void fetch_data_into_array(View view)
+    {
+
+        class  dbManager extends AsyncTask<String,Void,String>
+        {
+            protected void onPostExecute(String data)
+            {
+                try {
+                    JSONArray ja = new JSONArray(data);
+                    JSONObject jo = null;
+
+                    name = new String[ja.length()];
+                    desig = new String[ja.length()];
+                    img = new String[ja.length()];
+
+                    for (int i = 0; i < ja.length(); i++) {
+                        jo = ja.getJSONObject(i);
+                        name[i] = jo.getString("step");;
+                        desig[i] = jo.getString("description");
+                        img[i] ="http://10.0.2.2/instructions/images/" + jo.getString("image");;
+                    }
+
+                    myadapter adptr = new myadapter(getApplicationContext(), name, desig, img);
+                    lv.setAdapter(adptr);
+
+                } catch (Exception ex) {
+                    Toast.makeText(getApplicationContext(), ex.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            protected String doInBackground(String... strings)
+            {
+                try {
+                    URL url = new URL(strings[0]);
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+                    StringBuffer data = new StringBuffer();
+                    String line;
+
+                    while ((line = br.readLine()) != null) {
+                        data.append(line + "\n");
+                    }
+                    br.close();
+
+                    return data.toString();
+
+                } catch (Exception ex) {
+                    return ex.getMessage();
+                }
+
+            }
+
+        }
+        dbManager obj=new dbManager();
+        obj.execute(apiurl);
+
+    }
+
+    class myadapter extends ArrayAdapter<String>
+    {
+        Context context;
+        String ttl[];
+        String dsc[];
+        String rimg[];
+
+        myadapter(Context c, String ttl[], String dsc[], String rimg[])
+        {
+            super(c,R.layout.row,R.id.tv1,ttl);
+            context=c;
+            this.ttl=ttl;
+            this.dsc=dsc;
+            this.rimg=rimg;
+        }
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent)
+        {
+            LayoutInflater inflater=(LayoutInflater)getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View row=inflater.inflate(R.layout.row,parent,false);
+
+            ImageView img=row.findViewById(R.id.img1);
+            TextView tv1=row.findViewById(R.id.tv1);
+            TextView tv2=row.findViewById(R.id.tv2);
+
+
+            tv1.setText(ttl[position]);
+            tv2.setText(dsc[position]);
+
+            String url=rimg[position];
+
+
+            class ImageLoadTask extends AsyncTask<Void, Void, Bitmap> {
+                private String url;
+                private ImageView imageView;
+
+                public ImageLoadTask(String url, ImageView imageView) {
+                    this.url = url;
+                    this.imageView = imageView;
+                }
+
+                @Override
+                protected Bitmap doInBackground(Void... params) {
+                    try {
+                        URL connection = new URL(url);
+                        InputStream input = connection.openStream();
+                        Bitmap myBitmap = BitmapFactory.decodeStream(input);
+                        Bitmap resized = Bitmap.createScaledBitmap(myBitmap, 400, 400, true);
+                        return resized;
+                    } catch (Exception e) {
+                        Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_LONG).show();
+                    }
+                    return null;
+                }
+                @Override
+                protected void onPostExecute(Bitmap result) {
+                    super.onPostExecute(result);
+                    imageView.setImageBitmap(result);
+                }
+            }
+            ImageLoadTask obj=new ImageLoadTask(url,img);
+            obj.execute();
+
+            return row;
+        }
     }
 }
+
