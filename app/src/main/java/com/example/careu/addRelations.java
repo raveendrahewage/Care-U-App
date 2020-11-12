ff9568df52407680cea23b830c5d621516e64064
Damish <Damishnisal100@gmail.com>
2020-11-13 01:39:53 +0530

    added addRelations.java

--- app/src/main/java/com/example/careu/addRelations.java
+++ app/src/main/java/com/example/careu/addRelations.java
@@ -2,13 +2,94 @@ package com.example.careu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.basgeekball.awesomevalidation.AwesomeValidation;
+import com.basgeekball.awesomevalidation.ValidationStyle;
+
+import java.util.concurrent.ExecutionException;
 
 public class addRelations extends AppCompatActivity {
-
+    EditText _r1,_r1_num,_r2,_r2_num,_r3,_r3_num;
+    AwesomeValidation awesomeValidation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_relations);
+
+        _r1 = findViewById(R.id.r1);
+        _r1_num = findViewById(R.id.r1_num);
+        _r2 = findViewById(R.id.r2);
+        _r2_num = findViewById(R.id.r2_num);
+        _r3 = findViewById(R.id.r3);
+        _r3_num = findViewById(R.id.r3_num);
+
+    }
+
+
+    public void addRelation(View view) throws ExecutionException, InterruptedException {
+
+
+        awesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
+        String r1 = _r1.getText().toString();
+//        Toast.makeText(this,r1 ,Toast.LENGTH_LONG).show();
+        String r1_num = _r1_num.getText().toString();
+        if (!r1.isEmpty()){
+//           number(_r1_num.getText().toString(),R.id.r1_num);
+            awesomeValidation.addValidation(this,R.id.r1_num,"[0]{1}[7]{1}[1||2||5||6||7||8]{1}[0-9]{7}$",R.string.invalid_number1);
+        }
+
+        String r2 = _r2.getText().toString();
+        String r2_num = _r2_num.getText().toString();
+        if (!r2.isEmpty()){
+//            number(_r2_num.getText().toString(),R.id.r2_num);
+            awesomeValidation.addValidation(this,R.id.r2_num,"[0]{1}[7]{1}[1||2||5||6||7||8]{1}[0-9]{7}$",R.string.invalid_number1);
+        }
+
+        String r3 = _r3.getText().toString();
+        String r3_num = _r3_num.getText().toString();
+        if (!r3.isEmpty()){
+//            number(_r3_num.getText().toString(),R.id.r3_num);
+            awesomeValidation.addValidation(this,R.id.r3_num,"[0]{1}[7]{1}[1||2||5||6||7||8]{1}[0-9]{7}$",R.string.invalid_number1);
+        }
+
+        if (awesomeValidation.validate()) {
+            Toast.makeText(this,"Hellp" ,Toast.LENGTH_LONG).show();
+            sessionManagement sessionManagement1 = new sessionManagement(addRelations.this);
+            String userName = sessionManagement1.getSession();
+            String type = "AddRelation";
+            BackgroundWorker backgroundWorker = new BackgroundWorker(this);
+            String state =backgroundWorker.execute(type,userName,r1,r1_num,r2,r2_num,r3,r3_num).get();
+            Toast.makeText(this, state, Toast.LENGTH_LONG).show();
+            if (state.equals("Sucessfully add the Relatives")){
+
+                final Intent k = new Intent(this, homePageDuplicate.class);
+                final Intent l = new Intent(this, MainActivity.class);
+                //this.startActivity(i);
+                AlertDialog.Builder builder = new AlertDialog.Builder(addRelations.this);
+                builder.setMessage("Sucessfully add the Relatives");
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        startActivity(k);
+                    }
+                });
+
+                AlertDialog alert = builder.create();
+                alert.show();
+
+
+
+            }
+
+       }
+
+
     }
 }
