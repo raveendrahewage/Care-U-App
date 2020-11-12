aba283d262493840e1831a789569bf8ff49b8e89
LManjitha <lmanjithah@gmail.com>
2020-11-12 16:58:56 +0530

    send 1990 request

--- app/src/main/java/com/example/careu/myprofile.java
+++ app/src/main/java/com/example/careu/myprofile.java
@@ -12,6 +12,7 @@ public class myprofile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_profile);
+
     }
 
     public void my_requests(View view) {
